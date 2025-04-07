845dc8fc07d755f4d28f015a14af88245f7f9efc
Hellmessage <617494454@qq.com>
2025-04-08 01:01:58 +0800

    Tcp禁用UseNagleAlgorithm

---
+++ H9e.Core/H9eDynamicCall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace H9e.Core {
+    public class H9eDynamicCall {
+
+
+
+        public static object Call(string dllPath, string clazz, string method, params object[] argv) {
+            AppDomain newDomain = AppDomain.CreateDomain("DynamicAssemblyDomain");
+            try {
+                Assembly assembly = newDomain.Load(AssemblyName.GetAssemblyName(dllPath));
+                Type type = assembly.GetType(Path.GetFileNameWithoutExtension(dllPath));
+                MethodInfo methodInfo = type.GetMethod(method);
+                object instance = Activator.CreateInstance(type);
+                object result = methodInfo.Invoke(instance, argv);
+                return result;
+            } finally {
+                AppDomain.Unload(newDomain);
+            }
+        }
+
+    }
+}
--- H9e.Tcp/H9eTcpUtils.cs
+++ H9e.Tcp/H9eTcpUtils.cs
@@ -9,6 +9,7 @@ namespace H9e.Tcp {
         public static void Init() {
             System.Net.ServicePointManager.Expect100Continue = false;
             System.Net.ServicePointManager.DefaultConnectionLimit = 500;
+            System.Net.ServicePointManager.UseNagleAlgorithm = false;
             H9eTcpPacket.RegisterPacket<FileTcpPacket>("file");
             H9eTcpPacket.RegisterPacket<SystemTcpPacket>("system");
         }
