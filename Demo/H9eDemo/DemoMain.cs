1db98b88b7fd0f020d20684a9b76b0b71fad9a4d
Hellmessage <617494454@qq.com>
2025-04-09 21:26:13 +0800

    新增测试程序以及Database类

---
+++ Demo/H9eDemo/DemoMain.cs
@@ -0,0 +1,39 @@
+﻿using H9e.Core;
+using System.Reflection;
+
+namespace H9eDemo {
+    public partial class DemoMain : Form {
+        public DemoMain() {
+            InitializeComponent();
+
+            //H9eDynamicCall.StaticCall(AppDomain.CurrentDomain.BaseDirectory + "\\HellLib.Chromium.134.0.6998.36.x86.dll", "HellLib.Chromium.Version_134_0_6998_36_x86.Install", "Start");
+
+
+        }
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ H9e.Database/SQLBuilder/H9eDBPage.cs
@@ -0,0 +1,25 @@
+﻿namespace H9e.Database.SQLBuilder {
+    public class H9eDBPage {
+        public int Number { get; set; }
+        public int Size { get; set; }
+
+        private H9eDBPage(int number, int size) {
+            Number = number;
+            Size = size;
+        }
+
+        public override string ToString() {
+            if (Number < 0) {
+                Number = 0;
+            }
+            if (Size < 0) {
+                Size = 1;
+            }
+            return $"LIMIT {Size} OFFSET {Number * Size}";
+        }
+
+        public static H9eDBPage Limit(int number, int size) {
+            return new H9eDBPage(number, size);
+        }
+    }
+}
