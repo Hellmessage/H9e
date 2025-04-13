228f16f5778788230316c8904b33834c7d8b6d02
Hellmessage <617494454@qq.com>
2025-04-14 02:02:09 +0800

    新增MD5

---
+++ H9e.Core/H9eMD5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace H9e.Core {
+    public class H9eMD5 {
+
+
+        public static string Encode(string input) {
+            using (MD5 md5 = MD5.Create()) {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++) {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+    }
+}
